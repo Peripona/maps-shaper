69527cb868493d235c6493224c5c00492662d56e
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-09 09:08:20 +0100

    real path

--- src/App.js
+++ src/App.js
@@ -12,9 +12,22 @@ class App extends Component {
   googleMapsLoaded() {
     console.log("loaded", this);
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 44.5452, lng: -78.5389},
-      zoom: 9
+      center: {lng: 8.629650948685676, lat: 50.08116223910306},
+      zoom: 18
     });
+
+    const rs = {
+      "id": "u32v4",
+      "lat": 52.94786,
+      "lng": 12.39535,
+      "bounds": {
+        "topLeftLat": 52.94786,
+        "topLeftLng": 12.39535,
+        "bottomRightLat": 52.94786,
+        "bottomRightLng": 12.39535,
+      }
+    };
+
 
     const bounds = {
       north: 44.599,
@@ -23,12 +36,43 @@ class App extends Component {
       west: -78.649
     };
 
+    const coords = [
+      {lng: 8.629650948685676, lat: 50.08116223910306},
+      {lng: 8.629671658685494, lat: 50.081074029102965},
+      {lng: 8.629572198685212, lat: 50.08106470910294},
+      {lng: 8.62957882868522, lat: 50.081035179102926},
+      {lng: 8.629601258685181, lat: 50.08103741910297},
+      {lng: 8.629617238685144, lat: 50.080970109102864},
+      {lng: 8.629364698684299, lat: 50.08094527910298},
+      {lng: 8.629123288683497, lat: 50.08092189910306},
+      {lng: 8.6291077086835, lat: 50.08098818910321},
+      {lng: 8.629199958683834, lat: 50.08099724910314},
+      {lng: 8.629178848684061, lat: 50.08108606910335},
+      {lng: 8.62925390868421, lat: 50.08109342910326},
+      {lng: 8.629245948684373, lat: 50.081127919103324},
+      {lng: 8.629516098685285, lat: 50.08115384910315},
+      {lng: 8.62951713868525, lat: 50.08114938910319},
+      {lng: 8.629650948685676, lat: 50.08116223910306}
+    ];
+
+    // Construct the polygon.
+    const polygon = new window.google.maps.Polygon({
+      paths: coords,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      editable: true
+    });
+    polygon.setMap(map);
+/*
     // Define a rectangle and set its editable property to true.
     const rectangle = new window.google.maps.Rectangle({
       bounds: bounds,
       editable: true
     });
-    rectangle.setMap(map);
+    rectangle.setMap(map);*/
   }
 
   componentDidMount() {
--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work offline and load faster
+// you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+/*POLYGON((
+  {lat: 8.629650948685676, lng: 50.08116223910306},
+{lat: 8.629671658685494, lng: 50.081074029102965},
+{lat: 8.629572198685212, lng: 50.08106470910294},
+{lat: 8.62957882868522, lng: 50.081035179102926},
+{lat: 8.629601258685181, lng: 50.08103741910297},
+{lat: 8.629617238685144, lng: 50.080970109102864},
+{lat: 8.629364698684299, lng: 50.08094527910298},
+{lat: 8.629123288683497, lng: 50.08092189910306},
+{lat: 8.6291077086835, lng: 50.08098818910321},
+{lat: 8.629199958683834, lng: 50.08099724910314},
+{lat: 8.629178848684061, lng: 50.08108606910335},
+{lat: 8.62925390868421, lng: 50.08109342910326},
+{lat: 8.629245948684373, lng: 50.081127919103324},
+{lat: 8.629516098685285, lng: 50.08115384910315},
+{lat: 8.62951713868525, lng: 50.08114938910319},
+{lat: 8.629650948685676, lng: 50.08116223910306}
+))*/
